9e234a4877b1488b616362a43be36a06eb829922
Nadya <65254064+nadya-web@users.noreply.github.com>
2021-04-26 22:01:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,64 @@
 import pygame
 from os import path
 
+
 pygame.init()
 window = pygame.display.set_mode((1000, 1000))
-img_dir = path.join(path.dirname("N:\python\Sci-fic-game\main.py"), 'walkSprites')
-bg_dir = path.join(path.dirname("N:\python\Sci-fic-game\main.py"), 'bg')
 running = True
 
-walk_right = [pygame.image.load(path.join(img_dir, 'r1.png')), pygame.image.load(path.join(img_dir, 'r2.png')), pygame.image.load(path.join(img_dir, 'r3.png')),
-              pygame.image.load(path.join(img_dir, 'r4.png')), pygame.image.load(path.join(img_dir, 'r5.png')), pygame.image.load(path.join(img_dir, 'r6.png')),
-              pygame.image.load(path.join(img_dir, 'r7.png')), pygame.image.load(path.join(img_dir, 'r8.png')), pygame.image.load(path.join(img_dir, 'r9.png')),
-              pygame.image.load(path.join(img_dir, 'r10.png'))]
-walk_left = [pygame.image.load(path.join(img_dir, 'l1.png')), pygame.image.load(path.join(img_dir, 'l2.png')), pygame.image.load(path.join(img_dir, 'l3.png')),
-              pygame.image.load(path.join(img_dir, 'l4.png')), pygame.image.load(path.join(img_dir, 'l5.png')), pygame.image.load(path.join(img_dir, 'l6.png')),
-              pygame.image.load(path.join(img_dir, 'l7.png')), pygame.image.load(path.join(img_dir, 'l8.png')), pygame.image.load(path.join(img_dir, 'l9.png')),
-              pygame.image.load(path.join(img_dir, 'l10.png'))]
+img_dir = path.join(path.dirname("N:\python\Sci-fic-game\main.py"), 'walkSprites')
+bg_dir = path.join(path.dirname("N:\python\Sci-fic-game\main.py"), 'bg')
 
+walk_right = list(map(lambda i: pygame.image.load(path.join(img_dir, f'r{i}.png')), range(1, 12)))
+walk_left = list(map(lambda i: pygame.image.load(path.join(img_dir, f'l{i}.png')), range(1, 12)))
 
-player_properties = {
-    'velocity': 5,
-    'x': 100,
-    'y': 700,
-    'is_jump': False,
-    'jump_count': 10,
-    'left': False,
-    'right': False,
-    'steps': 0,
-    'side': ''
-}
+class player(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.velocity = 5
+        self.is_jump = False
+        self.jump_count = 10
+        self.left = False
+        self.right = False
+        self.steps = 0
+        self.side = 'right'
+
+    def draw(self, window):
+        if self.steps + 1 >= 30:
+            self.steps = 0
+        if self.right :
+            player = walk_right[self.steps // 3]
+            player.set_colorkey((255, 255, 255))
+            window.blit(player, (self.x, self.y))
+            self.steps += 1
+        elif self.left:
+            player = walk_left[self.steps // 3]
+            player.set_colorkey((255, 255, 255))
+            window.blit(player, (self.x, self.y))
+            self.steps += 1
+        else:
+            if self.side == 'left' :
+                player = walk_left[0]
+                player.set_colorkey((255, 255, 255))
+                window.blit(player, (self.x, self.y))
+            elif self.side == 'right' :
+                player = walk_right[0]
+                player.set_colorkey((255, 255, 255))
+                window.blit(player, (self.x, self.y))
+
+player = player(100, 700)
 
 def reDraw():
-    global player_properties
     bg = pygame.image.load(path.join(bg_dir, 'bg.png'))
     bg = pygame.transform.scale(bg, (1000, 1000))
-    window.blit(bg, (0,0))
-
-    if player_properties['steps'] >= 33:
-        player_properties['steps'] = 0
-    else:
-        if player_properties['right']:
-            window.blit(walk_right[player_properties['steps'] // 3 - 1], (player_properties['x'], player_properties['y']))
-            player_properties['steps'] += 1
-        elif player_properties['left'] :
-            window.blit(walk_left[player_properties['steps'] // 3 - 1], (player_properties['x'], player_properties['y']))
-            player_properties['steps'] += 1
-        else:
-            if player_properties['side'] == 'left':
-                window.blit(walk_left[0], (player_properties['x'], player_properties['y']))
-            elif player_properties['side'] == 'right':
-                window.blit(walk_right[0], (player_properties['x'], player_properties['y']))
-
-
+    window.blit(bg, (0, 0))
+    player.draw(window)
     pygame.display.update()
 
 clock = pygame.time.Clock()
 while running:
-    clock.tick(33)
+    clock.tick(30)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -63,34 +66,34 @@ while running:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_d]:
-        player_properties['x'] += player_properties['velocity']
-        player_properties['right'] = True
-        player_properties['left'] = False
-        player_properties['side'] = 'right'
+        player.x += player.velocity
+        player.right = True
+        player.left = False
+        player.side = 'right'
     elif keys[pygame.K_a]:
-        player_properties['x'] -= player_properties['velocity']
-        player_properties['left'] = True
-        player_properties['right'] = False
-        player_properties['side'] = 'left'
+        player.x -= player.velocity
+        player.left = True
+        player.right = False
+        player.side = 'left'
     else:
-        player_properties['right'] = False
-        player_properties['left'] = False
-        player_properties['steps'] = 0
+        player.right = False
+        player.left = False
+        player.steps = 0
     if keys[pygame.K_SPACE]:
-        player_properties['is_jump'] = True
-        player_properties['right'] = False
-        player_properties['left'] = False
-        player_properties['steps'] = 0
-    if player_properties['is_jump']:
-        if player_properties['jump_count'] >= -10:
+        player.is_jump = True
+        player.right = False
+        player.left = False
+        player.steps = 0
+    if player.is_jump:
+        if player.jump_count >= -10:
             num = 1
-            if player_properties['jump_count'] < 0:
+            if player.jump_count < 0:
                 num = -1
-            player_properties['y'] -= (player_properties['jump_count'] ** 2) * 0.5 * num
-            player_properties['jump_count'] -= 1
+            player.y -= (player.jump_count ** 2) * 0.5 * num
+            player.jump_count -= 1
         else:
-            player_properties['is_jump'] = False
-            player_properties['jump_count'] = 10
+            player.is_jump = False
+            player.jump_count = 10
 
     reDraw()
 pygame.quit()
