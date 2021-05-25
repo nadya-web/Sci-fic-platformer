861dbc02ddfe7ed97b9bb1e12dc9c57034c641a0
Nadya <65254064+nadya-web@users.noreply.github.com>
2021-05-25 19:24:47 +0300

    level editor added to the main file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from os import path
 import random
+import csv
 
 
 pygame.init()
@@ -15,6 +16,14 @@ enemy_death_dir = path.join(path.dirname("N:\python\Sci-fic-game\main.py"), 'ene
 pics_dir = path.join(path.dirname("N:\python\Sci-fic-game\main.py"), 'pics')
 bg_dir = path.join(path.dirname("N:\python\Sci-fic-game\main.py"), 'bg')
 
+ground = pygame.image.load(path.join(bg_dir, 'ground.png'))
+ground = pygame.transform.scale(ground, (2300, 1000))
+sky = pygame.image.load(path.join(bg_dir, 'sky.png'))
+sky = pygame.transform.scale(sky, (2300, 1000))
+mountaines1 = pygame.image.load(path.join(bg_dir, 'mountaines1.png'))
+mountaines1 = pygame.transform.scale(mountaines1, (2300, 1000))
+mountaines2 = pygame.image.load(path.join(bg_dir, 'mountaines2.png'))
+mountaines2 = pygame.transform.scale(mountaines2, (2300, 1000))
 pl_walk_right = list(map(lambda i: pygame.image.load(path.join(pl_walk_sprites_dir, f'r{i}.png')), range(1, 12)))
 pl_walk_left = list(map(lambda i: pygame.image.load(path.join(pl_walk_sprites_dir, f'l{i}.png')), range(1, 12)))
 pl_standing_right = list(map(lambda i: pygame.image.load(path.join(standing_sprites_dir, f'r{i}.png')), range(1, 12)))
@@ -22,14 +31,62 @@ pl_standing_left = list(map(lambda i: pygame.image.load(path.join(standing_sprit
 pl_death = list(map(lambda i: pygame.image.load(path.join(pl_death_dir, f'{i}.png')), range(1, 27)))
 enemy_death = list(map(lambda i: pygame.image.load(path.join(enemy_death_dir, f'{i}.png')), range(2, 21)))
 health_bars = list(map(lambda i: pygame.image.load(path.join(pics_dir, f'{i}.png')), range(1,7)))
+blocks = list(map(lambda i: pygame.image.load(path.join(pics_dir, f'block{i}.png')), range(1,13)))
 avatar = pygame.image.load(path.join(pics_dir, 'avatar.png'))
 btn = pygame.image.load(path.join(pics_dir, 'start_btn.png'))
 logo = pygame.image.load(path.join(pics_dir, 'logo.png'))
+enemy = pygame.image.load(path.join(pics_dir, 'r1.png'))
+fuel = pygame.image.load(path.join(pics_dir, 'fuel.png'))
+pl = pygame.image.load(path.join(pics_dir, 'pl.png'))
+
 
 btn = pygame.transform.scale(btn, (232, 96))
 logo = pygame.transform.scale(logo, (595, 230))
-btn.set_colorkey((255, 255, 255))
-logo.set_colorkey((255, 255, 255))
+
+ROWS = 16
+MAX_COLS = 150
+TILE_SIZE = 64
+level = 2
+
+SCROLL_FRAME = 200
+scroll = 0
+bg_scroll = 0
+
+TILES = blocks
+TILES.append(enemy)
+TILES.append(fuel)
+TILES.append(pl)
+
+class World():
+    def __init__(self):
+        self.list = list()
+
+    def process_data(self, data):
+        global enemies
+        player = 0
+        for y, row in enumerate(data):
+            for x, tile in enumerate(row):
+                if tile > -1:
+                    img = TILES[tile]
+                    coords = [x * 64, y * 64]
+                    if tile >= 0 and tile < 12:
+                        self.list.append([img, coords])
+                    elif tile == 12:
+                        enemy = Enemy(x * TILE_SIZE - 50, y * TILE_SIZE - 43, x * TILE_SIZE + 100)
+                        enemies.append(enemy)
+                    elif tile == 13:
+                        fuel = Fuel(img, x * TILE_SIZE, y * TILE_SIZE)
+                        fuels.append(fuel)
+                    elif tile == 14 and player == 0:
+                        player = Player(x * TILE_SIZE - 192, y * TILE_SIZE - 114)
+        return player
+
+    def draw(self):
+        global scroll
+        for tile in self.list:
+            tile[1][0] += scroll
+            window.blit(tile[0], tile[1])
+
 
 class Player(object):
     def __init__(self, x, y):
@@ -37,7 +94,7 @@ class Player(object):
         self.y = y
         self.width = 256
         self.hight = 256
-        self.velocity = 5
+        self.velocity = 10
         self.is_jump = False
         self.jump_count = 10
         self.left = False
@@ -107,13 +164,14 @@ class Player(object):
         if self.life < 5:
             self.life += 1
 
+
 class Bullet(object):
     def __init__(self, x, y, side):
         self.x = x
         self.y = y
         self.rad = 4
         self. color = ((226, 223, 223))
-        self.velocity = 10 * side
+        self.velocity = 15 * side
         self.side = side
 
     def draw(self, window, color):
@@ -143,7 +201,7 @@ class Enemy(object):
     def draw(self, window):
         if self.life < 5:
             self.move()
-            if self.steps + 1 >= 24 :
+            if self.steps + 1 >= 24:
                 self.steps = 0
 
             if self.velocity > 0:
@@ -182,10 +240,12 @@ class Enemy(object):
                         self.particles.append(Particle(self.x + self.width // 2 + 25 * self.side, self.y + self.hight // 2 + 14, self.side))
                     self.shoot_loop = 1
         else:
-            if self.death + 1 < 20:
+            if self.death + 1 <= 19:
+                print('death')
                 enemy = enemy_death[self.death]
                 enemy.set_colorkey((255, 255, 255))
                 window.blit(enemy, (self.x, self.y))
+                self.death += 1
 
     def move(self):
         if self.velocity > 0:
@@ -222,11 +282,26 @@ class Particle(object):
     def draw(self, window, color):
         pygame.draw.circle(window, color, (self.x2, self.y2), self.rad)
 
+class Fuel(object):
+    def __init__(self, img, x, y):
+        self.x = x
+        self.y = y
+        self.img = img
+    def draw(self, img, x, y):
+        window.blit(img, (x, y))
+
+def draw_bg():
+    window.fill(((255, 255, 255)))
+    width = 2300
+    for x in range(3):
+        window.blit(sky, (x * width - bg_scroll * 0.01, 0))
+        window.blit(mountaines1, (x * width - bg_scroll * 0.5, 0))
+        window.blit(mountaines2, (x * width - bg_scroll * 0.7, 0))
+        window.blit(ground, (x * width - bg_scroll * 0.9, 0))
+
 def reDraw():
-    bg = pygame.image.load(path.join(bg_dir, 'bg.png'))
-    bg = pygame.transform.scale(bg, (1000, 1000))
-    window.blit(bg, (0, 0))
     if start_game:
+        world.draw()
         player.draw(window)
     for enemy in enemies:
         enemy.draw(window)
@@ -238,22 +313,37 @@ def reDraw():
         bullet.draw(window, ((226, 223, 223)))
     for particle in particles:
         particle.draw(window, ((226, 223, 223)))
+    for fuel in fuels:
+        fuel.draw(fuel.img, fuel.x, fuel.y)
     pygame.display.update()
 
 def restart_game():
-    global player, bullets, particles, enemies, shoot_loop
-    player = Player(100, 700)
+    global bullets, particles, enemies, shoot_loop
     bullets = list()
     particles = list()
-    enemies = [Enemy(400, 750, 600)]
     shoot_loop = 0
-player = Player(100, 700)
 bullets = list()
+fuels = list()
 particles = list()
-enemies = [Enemy(400, 750, 600)]
+enemies = list()
 shoot_loop = 0
 start_game = False
 clock = pygame.time.Clock()
+
+world_data = list()
+for row in range(ROWS):
+    r = [-1] * MAX_COLS
+    world_data.append(r)
+
+with open(f'level{level}_data.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for x, row in enumerate(reader):
+        for y, tile in enumerate(row):
+            world_data[x][y] = int(tile)
+
+world = World()
+player = world.process_data(world_data)
+
 while running:
 
     clock.tick(30)
@@ -265,26 +355,27 @@ while running:
             if 384 < mouse[0] < 616 and 550 < mouse[1] < 626:
                 start_game = True
 
+
     if not start_game:
 
-        bg = pygame.image.load(path.join(bg_dir, 'bg.png'))
-        bg = pygame.transform.scale(bg, (1000, 1000))
-        window.blit(bg, (0, 0))
+        draw_bg()
         window.blit(btn, (384, 550))
         window.blit(logo, (213, 280))
         pygame.display.update()
         restart_game()
 
     else:
+
         for bullet in bullets:
-            if bullet.x < 1000 and bullet.x > 0:
+            if bullet.x > 10:
                 bullet.x += bullet.velocity
-            if bullet.y - bullet.rad < enemies[0].hitbox[1] + enemies[0].hitbox[3] and bullet.y + bullet.rad > enemies[0].hitbox[1]:
-                if bullet.x + bullet.rad > enemies[0].hitbox[0] and bullet.x - bullet.rad < enemies[0].hitbox[0] + enemies[0].hitbox[2]:
-                    enemies[0].hit()
-                    bullets.pop(bullets.index(bullet))
             else:
                 bullets.pop(bullets.index(bullet))
+            for enemy in enemies:
+                if bullet.y - bullet.rad < enemy.hitbox[1] + enemy.hitbox[3] and bullet.y + bullet.rad > enemy.hitbox[1]:
+                    if bullet.x + bullet.rad > enemy.hitbox[0] and bullet.x - bullet.rad < enemy.hitbox[0] + enemy.hitbox[2]:
+                        enemy.hit()
+                        bullets.pop(bullets.index(bullet))
         for particle in particles:
             if ((particle.x2 - particle.x1)**2 + (particle.y2 - particle.y1)**2)**0.5 > particle.life:
                 particles.pop(particles.index(particle))
@@ -329,14 +420,15 @@ while running:
             player.left = False
             player.side = 1
         elif keys[pygame.K_a]:
-            player.x -= player.velocity
             player.left = True
             player.right = False
             player.side = -1
+            player.x -= player.velocity
         else:
             player.right = False
             player.left = False
             player.steps = 0
+            scroll = 0
         if keys[pygame.K_SPACE]:
             player.is_jump = True
             player.right = False
@@ -352,6 +444,11 @@ while running:
             else:
                 player.is_jump = False
                 player.jump_count = 10
+        # if player.x > 1000 - SCROLL_FRAME or player.x < 100:
+        #     scroll = player.velocity * player.side * -1
+        #     bg_scroll -= scroll
+        #     player.x -= player.velocity * player.side
 
+        draw_bg()
         reDraw()
 pygame.quit()
